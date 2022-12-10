53d9949915df6d8d62217a5575dd499ee0dbaf25
Fedi Nabli <fedinabli@gmail.com>
2022-12-10 22:06:21 +0100

    Added kid model

---
+++ backend/models/kidModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const kidSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: [4, 'Minimum name length must be 4 characters']
+  },
+  picture: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true,
+    default: 6
+  }
+}, {
+  timestamps: true
+})
+const Kid = mongoose.model('Kid', kidSchema)
+
+export default Kid
